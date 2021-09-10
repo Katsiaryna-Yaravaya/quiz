6cfbff0f9b4e714acf6239f20182c749fbe1908c
Ekaterina Budenis <katsiaryna.budzenis@exposit.com>
2021-09-10 17:42:16 +0300

    add question component

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,22 @@
+import './index.css'
+import Question from "./question";
+
 const App = () => {
+
     return (
-        <div>
+        <div className='app container'>
+            <h1 className='app__heading'>
+                COUNTRY QUIZ
+            </h1>
+
+            <form>
+                <Question content = 'question'/>
+
+                {/*<input type='submit'>a</input>*/}
+                {/*<input type='submit'>b</input>*/}
+                {/*<input type='submit'>c</input>*/}
+                {/*<input type='submit'>d</input>*/}
+            </form>
         </div>
     );
 }
---
+++ src/question/index.tsx
@@ -0,0 +1,8 @@
+import './index.css'
+
+const Question= (props: {content: string}) => {
+    return (
+        <h2 className="question">question</h2>
+    );
+}
+export default Question
