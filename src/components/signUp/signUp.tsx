e2cb92eeb5dbb67e34f5b46cee940abbbce84c4d
Ekaterina Budenis <katsiaryna.budzenis@exposit.com>
2021-10-15 17:16:45 +0300

    add useForm

--- src/components/signUp/signUp.tsx
+++ src/components/signUp/signUp.tsx
@@ -13,28 +13,27 @@ import {useEffect, useState} from "react";
 const SignUp = () => {
   const history = useHistory();
   const {register, handleSubmit, formState: {errors}, watch} = useForm({mode: 'onBlur'});
-  // const [errorMessage, setErrorMessage] = useState<string>('')
-  // "sign-up__user error"
 
   // useEffect(() => {
-  //   if (Object.keys(errors).length) {
-  //     setErrorMessage(' error')
-  //   } else {
-  //     setErrorMessage('')
-  //   }
+  //   getErrorMessage(watch())
   // }, [errors])
 
+  const name = watch('name')
+  const surName = watch('surName')
+  const age = watch('age')
+  const email = watch('pass')
+  const pass = watch('pass')
+  const repeatPassword = watch('repeatPassword')
+
   const getErrorMessage = (value) => {
-    if (value) {
+    console.log(!value)
+    console.log(Object.keys(errors).length)
+
+    if (!value && Object.keys(errors).length) {
       return "sign-up__user error"
     }
-    return "sign-up__user"
+      return "sign-up__user"
   }
-
-
-  const pass = watch('pass')
-
-  console.log(errors)
 
   const onSubmit = (data) => {
     addNewUser(data)
@@ -47,19 +46,19 @@ const SignUp = () => {
       <form className="quiz-form" onSubmit={handleSubmit(onSubmit)}>
 
         <div className="form__sign-up">
-          <input className={getErrorMessage} placeholder='Name' {...register('name', {
+          <input className={getErrorMessage(name)} placeholder='Name' {...register('name', {
             required: true,
             pattern: /[A-Za-z]/
           })}/>
           {errors.name && <span className="sign-up__user-error">Required field for entering in Latin letters</span>}
 
-          <input className={getErrorMessage} placeholder='Surname' {...register('surName', {
+          <input className={getErrorMessage(surName)} placeholder='Surname' {...register('surName', {
             required: true,
             pattern: /[A-Za-z]/
           })}/>
           {errors.surName && <span className="sign-up__user-error">Required field for entering in Latin letters</span>}
 
-          <input className={getErrorMessage} placeholder='Age' {...register('age', {
+          <input className={getErrorMessage(age)} placeholder='Age' {...register('age', {
             required: true,
             pattern: /\d?\d/,
             min: 2,
@@ -68,16 +67,16 @@ const SignUp = () => {
           })}/>
           {errors.age && <span className="sign-up__user-error">Required numeric field from 2 to 9</span>}
 
-          <input className={getErrorMessage} placeholder='E-mail'
+          <input className={getErrorMessage(email)} placeholder='E-mail'
                  autoComplete="username" {...register('email', {required: true, pattern: /^\w+@\w+\.\w{2,}$/})}/>
           {errors.email && <span className="sign-up__user-error">Required field for entering e-mail</span>}
 
-          <input className={getErrorMessage} placeholder='Password' type="password"
+          <input className={getErrorMessage(pass)} placeholder='Password' type="password"
                  autoComplete="new-password" {...register('pass', {required: true, minLength: 6})}/>
           {errors.pass &&
           <span className="sign-up__user-error">Required field for entering a password min 6 characters</span>}
 
-          <input className={getErrorMessage} type="password" placeholder='Repeat password'
+          <input className={getErrorMessage(repeatPassword)} type="password" placeholder='Repeat password'
                  autoComplete="new-password" {...register('repeatPassword', {
             required: true,
             validate: value => value === pass
